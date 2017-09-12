479cbd29133f9ae080d3a02a0e9f3a51f3f181c8
huangfuchunfeng <xpnsau@qq.com>
2017-09-12 20:07:49 +0800

    Add rest-assured testcase for testing multiple confings

--- src/test/java/com/ppdai/jiechu/testcases/TestCase.java
+++ src/test/java/com/ppdai/jiechu/testcases/TestCase.java
@@ -2,6 +2,17 @@ package com.ppdai.jiechu.testcases;
 
 import org.testng.annotations.Test;
 
+import io.restassured.RestAssured;
+import io.restassured.config.HeaderConfig;
+import io.restassured.config.LogConfig;
+import io.restassured.http.ContentType;
+
+import static io.restassured.RestAssured.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
 public class TestCase {
 
 	@Test(groups="test1")
@@ -17,9 +28,23 @@ public class TestCase {
 	}
 
 	@Test(groups="test3")
-	public void test3 () {
+	public void test3 () throws FileNotFoundException {
 		System.out.println(Thread.currentThread().getId());
 		System.out.println("##test3");
+		PrintStream fileOutPutStream = new PrintStream(new File("./restassred.txt"));
+		given().
+			proxy("localhost", 8888).
+			header("header1", "value1").
+			header("header1", "value2").
+			config(RestAssured.config().logConfig(new LogConfig().defaultStream(fileOutPutStream))).
+			config(RestAssured.config().headerConfig(new HeaderConfig().overwriteHeadersWithName("header1"))).
+			log().all().
+		when().
+			get("http://127.0.0.1:5000/hello").
+		then().
+			log().all().
+			assertThat().statusCode(200).
+			assertThat();
 	}
 	
 	
